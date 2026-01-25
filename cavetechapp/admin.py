41a007d1f0d23fe7fbbf07a8a1d3b07b2125ef33
ero034@post.uit.no <elisabethroynestad@Elisabeths-MacBook-Pro.local>
2026-01-25 22:25:43 +0100

    Make Site Settings auto-redirect to edit page in admin

--- cavetechapp/admin.py
+++ cavetechapp/admin.py
@@ -7,7 +7,8 @@ from .models import Person, Project, Category, SiteSettings
 
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
-    list_display = ('email', 'updated_at')
+    list_display = ('get_title', 'email', 'updated_at')
+    
     fieldsets = (
         ('About Us', {
             'fields': ('about_title', 'about_content', 'image')
@@ -25,6 +26,11 @@ class SiteSettingsAdmin(admin.ModelAdmin):
     )
     readonly_fields = ('created_at', 'updated_at')
 
+    def get_title(self, obj):
+        """Display title in list."""
+        return "Site Settings"
+    get_title.short_description = "Setting"
+
     def has_add_permission(self, request):
         """Allow adding if no SiteSettings exists."""
         return not SiteSettings.objects.exists()
@@ -36,6 +42,20 @@ class SiteSettingsAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         """Allow changing SiteSettings."""
         return True
+    
+    def changelist_view(self, request, extra_context=None):
+        """Redirect to the edit page if there's only one instance."""
+        response = super().changelist_view(request, extra_context)
+        try:
+            obj = SiteSettings.objects.first()
+            if obj and request.GET.get('q') is None:
+                # Automatically redirect to edit page
+                from django.urls import reverse
+                from django.shortcuts import redirect
+                return redirect(reverse('admin:cavetechapp_sitesettings_change', args=[obj.pk]))
+        except:
+            pass
+        return response
 
 
 @admin.register(Category)
